a45e93244e697400644ee5dd5cd3ff3a48691e31
TranChien321 <tranchienn2307@gmail.com>
2025-04-15 14:33:26 +0700

    main

---
+++ Module_1/Lesson_9/Bai_tap_lesson_9/JS/may_tinh.js
@@ -0,0 +1,9 @@
+function inputNumber(value) {
+    document.getElementById("spreadsheet").value += value;
+}
+function inputAllowable(value) {
+    document.getElementById("spreadsheet").value += value;
+}
+function deLete() {
+    document.getElementById("spreadsheet").value = ""
+}
