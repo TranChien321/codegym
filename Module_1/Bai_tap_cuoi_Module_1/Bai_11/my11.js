5e2cb4ae796a43f3c053d8b4eba46d15607c5da5
TranChien321 <tranchienn2307@gmail.com>
2025-05-05 10:29:17 +0700

    main

--- Module_1/Bai_tap_cuoi_Module_1/Bai_11/my11.js
+++ Module_1/Bai_tap_cuoi_Module_1/Bai_11/my11.js
@@ -2,7 +2,7 @@ let listStudent = [
     ['HV-0001', 'Nguyễn Văn A', 'C1022G1', 'nguyenvana@gmail.com', '01/01/2004'],
     ['HV-0002', 'Nguyễn Văn B', 'C1022G1', 'nguyenvanb@gmail.com', '02/02/2004'],
     ['HV-0003', 'Nguyễn Văn C', 'C1022G1', 'nguyenvanc@gmail.com', '03/03/2004'],
-]
+];
 
 function showStudentList() {
     let html = "";
@@ -13,8 +13,8 @@ function showStudentList() {
             <td>${item[2]}</td>
             <td>${item[3]}</td>
             <td>${item[4]}</td>
-        </tr>`
-    })
+        </tr>`;
+    });
     document.getElementById("container-student").innerHTML = html;
 }
 
@@ -27,45 +27,52 @@ function addStudent() {
     let email = prompt("Nhập email");
     let date = prompt("Nhập ngày sinh");
 
-    if (listStudent.some(item => item[0] === id)) {
-        alert("Mã học viên đã tồn tại");
+    if (!/^HV-\d{4}$/.test(id)) {
+        alert("Mã học viên không đúng định dạng HV-XXXX");
         return;
     }
-    let idRegex = /^HV-\d{4}$/;
-    if (!idRegex.test(id)) {
-        alert("Mã học viên không đúng định dạng HV-XXXX (với XXXX là 4 chữ số).");
-        return;
-    }
-    if (listStudent.some(item => item[3] === email)) {
-        alert("Email đã tồn tại");
-        return;
+    for (let i = 0; i < listStudent.length; i++) {
+        if (listStudent[i][0] === id) {
+            alert("Mã học viên đã tồn tại");
+            return;
+        }
+        if (listStudent[i][3] === email) {
+            alert("Email đã tồn tại");
+            return;
+        }
     }
     if (name.length > 50) {
-        alert("Tên học viên không được quá 50 ký tự.");
+        alert("Tên học viên không được quá 50 ký tự");
         return;
     }
-    if (id.value === "" || name.value === "" || className.value === "" || email.value === "" || date.value === "") {
-        alert("Thông tin không được để trống");
+    if (!date.match(/^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/)) {
+        alert("Ngày sinh phải đúng định dạng dd/MM/yyyy");
         return;
     }
-    let dateRegex = /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
-    if (!dateRegex.test(date)) {
-        alert("Ngày sinh phải đúng định dạng dd/MM/yyyy.");
+    if (id === "" || name === "" || className === "" || email === "" || date === "") {
+        alert("Không được để trống thông tin");
         return;
     }
+
     listStudent.push([id, name, className, email, date]);
     alert("Thêm học viên thành công");
-    showStudentList()
+    showStudentList();
 }
 
 function deleteStudent() {
     let id = prompt("Nhập mã học viên cần xóa");
-    let index = listStudent.findIndex(item => item[0] === id);
+    let index = -1;
+    for (let i = 0; i < listStudent.length; i++) {
+        if (listStudent[i][0] === id) {
+            index = i;
+            break;
+        }
+    }
     if (index === -1) {
         alert("Không tìm thấy mã học viên");
         return;
     }
-    if (confirm("Bạn có chắc chắn muốn xóa không?")) {
+    if (confirm("Bạn có chắc muốn xóa không?")) {
         listStudent.splice(index, 1);
         alert("Xóa học viên thành công");
         showStudentList();
@@ -74,24 +81,27 @@ function deleteStudent() {
 
 function editStudent() {
     let id = prompt("Nhập mã học viên cần sửa");
-    let index = listStudent.findIndex(item => item[0] === id);
+    let index = -1;
+    for (let i = 0; i < listStudent.length; i++) {
+        if (listStudent[i][0] === id) {
+            index = i;
+            break;
+        }
+    }
     if (index === -1) {
         alert("Không tìm thấy mã học viên");
         return;
     }
     let name = prompt("Nhập tên học viên", listStudent[index][1]);
-    let className = prompt("Nhập tên lớp học", listStudent[index][2]);
+    let className = prompt("Nhập tên lớp", listStudent[index][2]);
     let email = prompt("Nhập email", listStudent[index][3]);
     let date = prompt("Nhập ngày sinh", listStudent[index][4]);
-    if (listStudent.some(item => item[0] === id && item[1] === name)) {
-        alert("Mã học viên đã tồn tại");
-        return;
-    }
-    if (id.value === "" || name.value === "" || className.value === "" || email.value === "" || date.value === "") {
-        alert("Thông tin không được để trống");
+
+    if (id === "" || name === "" || className === "" || email === "" || date === "") {
+        alert("Không được để trống thông tin");
         return;
     }
     listStudent[index] = [id, name, className, email, date];
-    alert("Sửa học viên thành công");
+    alert("Sửa thông tin thành công");
     showStudentList();
 }
