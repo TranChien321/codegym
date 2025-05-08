ad26cbebe5c0cd6b051c8e4ffc1f25984f2aeb8c
TranChien321 <tranchienn2307@gmail.com>
2025-05-08 14:23:52 +0700

    codegymjava

--- Module_2/Lesson_1/HelloWord.java
+++ Module_2/Lesson_1/HelloWord.java
@@ -1,6 +1,6 @@
 package Lesson_1;
 
-public class Bai_1 {
+public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
---
+++ Module_2/Lesson_1/SystemTime.java
@@ -0,0 +1,11 @@
+package Lesson_1;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Current date and time: " + now);
+    }
+}
