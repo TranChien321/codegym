d70a0a768dcfd0ddfa0fe89c4168cba97470ae17
TranChien321 <tranchienn2307@gmail.com>
2025-05-09 14:58:13 +0700

    codygymjava

---
+++ Module_2/Lesson_2/DisplayGeometry.java
@@ -0,0 +1,58 @@
+package Lesson_2;
+
+import java.util.Scanner;
+
+public class DisplayGeometry {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+        while (choice != 0) {
+            System.out.println("1. Hình Chữ Nhật");
+            System.out.println("2. Tam Giác Vuông Botton");
+            System.out.println("3. Tam Giác Vuông Top");
+            System.out.println("4. Tam Giác Cân");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Hình Chữ Nhật");
+                    System.out.println("* * * * * * *");
+                    System.out.println("* * * * * * *");
+                    System.out.println("* * * * * * *");
+                    System.out.println("* * * * * * *");
+                    break;
+                case 2:
+                    System.out.println("Tam Giác Vuông Botton");
+                    System.out.println("*");
+                    System.out.println("* *");
+                    System.out.println("* * * ");
+                    System.out.println("* * * * ");
+                    System.out.println("* * * * * ");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("Tam Giác Vuông Top");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * ");
+                    System.out.println("* * * * ");
+                    System.out.println("* * * ");
+                    System.out.println("* *");
+                    System.out.println("*");
+                    break;
+                case 4:
+                    System.out.println("Tam Giác Cân");
+                    System.out.println("      *      ");
+                    System.out.println("     * *     ");
+                    System.out.println("    * * *    ");
+                    System.out.println("   * * * *   ");
+                    System.out.println("  * * * * *  ");
+                    System.out.println(" * * * * * * ");
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+        }
+    }
+}
---
+++ Module_2/Lesson_2/DisplayPrime.java
@@ -0,0 +1,28 @@
+package Lesson_2;
+
+public class DisplayPrime {
+    public static void main(String[] args) {
+        int number = 2;
+        int count = 0;
+
+        while (count < 20) {
+            if (isPrime(number)) {
+                System.out.println(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
