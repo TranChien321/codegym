af95727d5d22e0ebe7db0fb42217b454edb4fa9f
TranChien321 <tranchienn2307@gmail.com>
2025-05-13 14:05:39 +0700

    codegymjava

---
+++ Module_2/Lesson_4/Main.java
@@ -0,0 +1,19 @@
+package Lesson_4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter rectangle width: ");
+        double width = scanner.nextDouble();
+
+        System.out.print("Enter rectangle height: ");
+        double height = scanner.nextDouble();
+
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Area = " + rectangle.getArea());
+        System.out.println("Perimeter = " + rectangle.getPerimeter());
+    }
+}
